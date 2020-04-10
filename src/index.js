dac0541202efe05733d4a2b83a75672cdceae809
Yash Solanki <yashsolanki777@gmail.com>
2020-04-10 14:04:40 +0530

    lodash correction

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyB1AWx9-l_xpy1ln_PvynyTI7KzAOQulr8";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            slectedVideo: null,
+        };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.videoSearch("ferrari");
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0],
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term);
+        }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={(selectedVideo) =>
+                        this.setState({ selectedVideo })
+                    }
+                    videos={this.state.videos}
+                />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
